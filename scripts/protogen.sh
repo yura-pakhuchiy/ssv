dc0fe8f47ae45d9be52f094e10882dab6bce6f13
systemblox <systemblox@blox.io>
2021-04-13 17:13:53 +0300

    Setup ci/cd

--- scripts/protogen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-# Generate protoc script
-# We use the vendor dir to link 3rd party dependencies into the generate proto files.
-# It is necessary to delete the vendor dir when we finish
-
-go generate $GOPATH/src/github.com/bloxapp/ssv/ibft/proto/generate.go
--- storage/storage.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import "github.com/bloxapp/ssv/ibft/proto"
-
-// Storage is an interface for persisting chain data
-type Storage interface {
-	SavePrepared(signedMsg *proto.SignedMessage)
-	SaveDecided(signedMsg *proto.SignedMessage)
-}
